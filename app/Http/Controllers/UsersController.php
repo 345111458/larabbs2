6a904f6c9e3f96b74e87fe74944ea38a8f588854
345111458 <345111458@qq.com>
2019-05-29 07:23:34 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,10 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+	public function __construct(){
+
+		$this->middleware('auth',['except'=>['show']]);
+	}
     //
     //
     public function show(User $user){
@@ -19,7 +23,7 @@ class UsersController extends Controller
 
 
     public function edit(User $user){
-
+    	$this->authorize('update',$user);
 
     	return view('users.edit',compact('user'));
     }
@@ -27,6 +31,8 @@ class UsersController extends Controller
 
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
 
+    	$this->authorize('update',$user);
+    	
     	$data = $request->all();
 
     	if ($request->avatar) {
