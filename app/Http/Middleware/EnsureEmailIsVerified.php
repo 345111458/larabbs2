fe6f5eba73a681fe5746eb1cd80a8491802b0882
345111458 <345111458@qq.com>
2019-05-29 06:12:32 +0000

    用户个人页面原型

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -17,8 +17,7 @@ class EnsureEmailIsVerified
     {
         if ($request->user() && !$request->user()->hasVerifiedEmail() && !$request->is('email/*','logout')) {
              // 根据客户端返回对应的内容
-            return $request->
-            () ? abort(403, 'Your email address is not verified.') : redirect()->route('verification.notice');
+            return $request->expectsJson() ? abort(403, 'Your email address is not verified.') : redirect()->route('verification.notice');
         }
         return $next($request);
     }
