e8775f12e8bc4dc9e16ccae686fb8a2acd7b483c
345111458 <345111458@qq.com>
2019-05-29 05:58:41 +0000

    找回密码成功消息提示

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -17,7 +17,8 @@ class EnsureEmailIsVerified
     {
         if ($request->user() && !$request->user()->hasVerifiedEmail() && !$request->is('email/*','logout')) {
              // 根据客户端返回对应的内容
-            return $request->expectsJson() ? abort(403, 'Your email address is not verified.') : redirect()->route('verification.notice');
+            return $request->
+            () ? abort(403, 'Your email address is not verified.') : redirect()->route('verification.notice');
         }
         return $next($request);
     }
